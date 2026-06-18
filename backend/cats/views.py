9a3941e738a0813aa6fc17a5bbb6092b77d8f8f6
Pavel Okhrim <paokhrim@gmail.com>
2026-06-18 11:15:37 +0300

    Add type hints to view methods for improved clarity and type safety

--- backend/cats/views.py
+++ backend/cats/views.py
@@ -1,9 +1,12 @@
 import logging
+from typing import Any
 
 from django.conf import settings
 from django.core.cache import cache
+from django.db.models import QuerySet
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.request import Request
 from rest_framework.response import Response
 
 from .cache import build_cat_list_cache_key, reset_cat_cache
@@ -17,10 +20,10 @@ class CatViewSet(viewsets.ModelViewSet):
     serializer_class = CatSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
 
-    def get_queryset(self):
+    def get_queryset(self) -> QuerySet[Cat]:
         return Cat.objects.with_relations()
 
-    def list(self, request, *args, **kwargs):
+    def list(self, request: Request, *args: Any, **kwargs: Any) -> Response:
         page_number = request.query_params.get('page', '1')
         cache_key = build_cat_list_cache_key(page_number)
         cached_payload = cache.get(cache_key)
@@ -30,17 +33,17 @@ class CatViewSet(viewsets.ModelViewSet):
         cache.set(cache_key, response.data, settings.CACHE_TTL)
         return response
 
-    def perform_create(self, serializer):
+    def perform_create(self, serializer: CatSerializer) -> None:
         cat = serializer.save(owner=self.request.user)
         reset_cat_cache()
         logger.info('Создан кот %s пользователем %s', cat.pk, self.request.user.pk)
 
-    def perform_update(self, serializer):
+    def perform_update(self, serializer: CatSerializer) -> None:
         cat = serializer.save()
         reset_cat_cache()
         logger.info('Обновлён кот %s пользователем %s', cat.pk, self.request.user.pk)
 
-    def perform_destroy(self, instance):
+    def perform_destroy(self, instance: Cat) -> None:
         cat_id = instance.pk
         owner_id = instance.owner_id
         super().perform_destroy(instance)
